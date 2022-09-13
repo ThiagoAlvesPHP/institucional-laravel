fc3baf850e2a2df98c85f811a97763f61a87b577
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-09-13 18:41:01 -0300

    efeito Scroll Suave para links internos, fechar menu ao clicar

--- public/assets/js/custom/navbar.js
+++ public/assets/js/custom/navbar.js
@@ -1,8 +1,20 @@
 $(function(){
     // fechar navbar
-    $(document).on('click', '.navbar .close', function(){
+    $(document).on('click', '.navbar .links .close', function(){
         $(this).parent().addClass('navbar-mobile-close');
         $(this).parent().removeClass('navbar-mobile-show');
+    });
+    $(document).on('click', '.navbar-mobile-show .link-menu', function(e) {
+        e.preventDefault();
+        let id = $(this).attr('href').split('#'),
+            targetOffset = $(`#${id[1]}`).offset().top;
+
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 800);
+
+        $('.navbar ul.links').addClass('navbar-mobile-close');
+        $('.navbar ul.links').removeClass('navbar-mobile-show');
     });
 
     // abrir navbar
