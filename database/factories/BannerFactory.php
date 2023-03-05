c3a6aae212aa11ed5c6bb00b1ef3e729bba48814
thiagoalvesdev <thicoalves@hotmail.com>
2023-03-05 16:26:56 -0300

    SEo

--- database/factories/BannerFactory.php
+++ database/factories/BannerFactory.php
@@ -17,7 +17,7 @@ class BannerFactory extends Factory
             'title'     => 'LT Developer',
             'text'      => 'Você precisa de um site profissional e moderno para o seu negócio? Nós somos especialistas em desenvolvimento web e podemos ajudá-lo a criar a presença online que sua empresa merece. Com anos de experiência em criação de sites, programação web e web design, sabemos o que é preciso para criar um site que reflita a identidade da sua marca e atraia clientes em potencial.',
             'link'      => 'https://wa.me/message/2CZCBDNW4HZJG1',
-            'link_text' => 'Clique Aqui',
+            'link_text' => 'Fale Conosco',
             'image'     => 'banner.png'
         ];
     }
